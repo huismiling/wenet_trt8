0f409c576f8bf78c9d4971adee318edec34b5492
xiaozhang <xiaozhang@123.com>
2022-05-01 09:41:43 +0800

    del onnxsim

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 
 python fix_encoder.py
 
-python onnxsim.py encoder_new.onnx encoder_sim.onnx 4  \
-    --dynamic-input-shape --input-shape "speech:16,256,80" "speech_lengths:16" 
+# python onnxsim.py encoder_new.onnx encoder_sim.onnx 4  \
+#     --dynamic-input-shape --input-shape "speech:16,256,80" "speech_lengths:16" 
 
-python onnxsim.py decoder.onnx decoder_sim.onnx 4  \
-    --dynamic-input-shape --input-shape "encoder_out:16,80,256" "encoder_out_lens:16" "hyps_pad_sos_eos:16,10,64" "hyps_lens_sos:16,10" "ctc_score:16,10"
+# python onnxsim.py decoder.onnx decoder_sim.onnx 4  \
+#     --dynamic-input-shape --input-shape "encoder_out:16,80,256" "encoder_out_lens:16" "hyps_pad_sos_eos:16,10,64" "hyps_lens_sos:16,10" "ctc_score:16,10"
 
 
 
 
 trtexec --workspace=3000 \
-        --onnx=./encoder_sim.onnx \
+        --onnx=./encoder_new.onnx \
         --optShapes=speech:4x64x80,speech_lengths:4 \
         --maxShapes=speech:16x256x80,speech_lengths:16 \
         --minShapes=speech:1x16x80,speech_lengths:1 \
