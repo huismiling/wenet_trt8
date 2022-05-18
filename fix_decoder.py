608cbcf35054a37f448d373b7489e30dd6f7e616
xiaozhang <xiaozhang@123.com>
2022-05-18 12:56:46 +0800

    up

--- fix_decoder.py
+++ fix_decoder.py
@@ -3,7 +3,7 @@ from onnx import helper
 from onnx import TensorProto
 import numpy as np
 
-mdl = onnx.load("decoder.onnx")
+mdl = onnx.load("/workspace/decoder.onnx")
 
 mdl.graph.input[2].type.tensor_type.shape.dim[2].dim_value = 64
 print(mdl.graph.input[2].type)
