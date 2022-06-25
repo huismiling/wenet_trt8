4ed0ba1eff771ebf0e76b653314bbfb677754189
tripleMu <865626@163.com>
2022-06-25 20:57:29 +0800

    Add other branch

---
+++ other_branch/quant_ppq/encoder_quant.py
@@ -0,0 +1,47 @@
+import torch
+import onnx
+import random
+from ppq import *
+from ppq.api import *
+from ppq.quantization.quantizer import TensorRTQuantizer
+from data import MyDataSet
+from torch.utils.data import DataLoader
+
+
+DEVICE = 'cuda'
+PLATFORM = TargetPlatform.TRT_INT8
+
+save = './encoder_quant.onnx'
+MODEL_PATH = './encoder_new.onnx'
+
+dataset = MyDataSet(mode='encoder')
+SAMPLES = DataLoader(dataset,
+                    batch_size=None,
+                    shuffle=True,
+                    num_workers=0)
+
+inp = None
+for _ in range(random.randint(1,100)):
+    inp =  next(iter(SAMPLES))
+print(inp)
+
+with ENABLE_CUDA_KERNEL():
+    QS = QuantizationSettingFactory.trt_setting()
+    ir = load_onnx_graph(onnx_import_file=MODEL_PATH)
+    for op in ir.operations.values():
+        if op.type in {'Unsqueeze', 'Squeeze', 'ReduceSum'}:
+            axes = op.inputs[1].value
+            ir.remove_variable(removing_var=op.inputs[1])
+            op.attributes['axes'] = axes.tolist()
+
+    qir = quantize_native_model(
+        model=ir, calib_dataloader=SAMPLES, calib_steps=300,
+        input_shape=None, inputs=inp,
+        platform=TargetPlatform.TRT_INT8, setting=QS)
+
+    graphwise_error_analyse(
+        graph=qir, running_device='cuda',
+        dataloader=SAMPLES)
+
+    export_ppq_graph(graph=qir, platform=TargetPlatform.TRT_INT8,
+                     graph_save_to=save)
---
+++ other_branch/quant_ppq/model_dynamic.py
@@ -0,0 +1,66 @@
+import onnx
+import onnx_graphsurgeon as gs
+import numpy as np
+import sys
+
+flag = sys.argv[1]
+
+ori = f'../../model/{flag}.onnx'
+
+
+ori_model = onnx.load(ori)
+gs_graph = gs.import_onnx(ori_model)
+
+nameIn1 = [i.name for i in gs_graph.inputs]
+nameOut1 = [i.name for i in gs_graph.outputs]
+
+model_pb = ori_model.graph
+
+dyDict = {'inpMess': {}, 'outMess': {}}
+for item in model_pb.input:
+    dyDict['inpMess'][item.name] = {}
+    shape = item.type.tensor_type.shape.dim
+    for i, j in enumerate(shape):
+        if j.dim_param:
+            dyDict['inpMess'][item.name][i] = j.dim_param
+for item in model_pb.output:
+    dyDict['outMess'][item.name] = {}
+    shape = item.type.tensor_type.shape.dim
+    for i, j in enumerate(shape):
+        if j.dim_param:
+            dyDict['outMess'][item.name][i] = j.dim_param
+
+
+
+save = f'./{flag}_quant.onnx'
+
+model = onnx.load(save)
+
+gs_graph = gs.import_onnx(model)
+
+nameIn2 = [i.name for i in gs_graph.inputs]
+nameOut2 = [i.name for i in gs_graph.outputs]
+
+idxIn = [nameIn2.index(i) for i in nameIn1]
+idxOut = [nameOut2.index(i) for i in nameOut1]
+
+
+gs_graph.outputs = [gs_graph.outputs[idx] for idx in idxOut]
+gs_graph.inputs = [gs_graph.inputs[idx] for idx in idxIn]
+
+gs_graph.cleanup().toposort()
+
+model = gs.export_onnx(gs_graph)
+model_pb = model.graph
+
+inpMess = model_pb.input
+outMess = model_pb.output
+
+if dyDict:
+    for k, v in dyDict.items():
+        for i in eval(k):
+            for index, val in v[i.name].items():
+                i.type.tensor_type.shape.dim[int(index)].dim_param = val
+
+save = f'./{flag}_quant_dynamic.onnx'
+onnx.save(model, save)
