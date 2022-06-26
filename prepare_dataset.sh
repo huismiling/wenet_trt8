2bf1c3165f696ffb28461c0fe2fdd91ca54dfe6f
tripleMu <865626@163.com>
2022-06-26 21:12:34 +0800

    Fix

--- prepare_dataset.sh
+++ prepare_dataset.sh
@@ -5,12 +5,16 @@ torch_model_url=https://oneflow-static.oss-cn-beijing.aliyuncs.com/tripleMu/tens
 encoder_url=https://oneflow-static.oss-cn-beijing.aliyuncs.com/tripleMu/tensorrt/encoder.onnx
 decocer_url=https://oneflow-static.oss-cn-beijing.aliyuncs.com/tripleMu/tensorrt/decoder.onnx
 npys_url=https://oneflow-static.oss-cn-beijing.aliyuncs.com/tripleMu/tensorrt/npys.tar
+data_url=https://oneflow-static.oss-cn-beijing.aliyuncs.com/tripleMu/tensorrt/data.tar.gz
 wget $test_data_url -P $repoPath/datasets/
 wget $npys_url -P $repoPath/datasets/
 wget $torch_model_url -P $repoPath/model/
 wget $encoder_url -P $repoPath/model/
 wget $decocer_url -P $repoPath/model/
+wget $data_url -P $repoPath/datasets/
 
 python prepare_dataset.py $repoPath
 cd ./datasets/
 tar -xvf npys.tar
+tar -zxvf data.tar.gz
+mv data ort_quant_data
